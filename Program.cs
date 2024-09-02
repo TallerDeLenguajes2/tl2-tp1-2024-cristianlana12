f5cf1cb1845bdaffd269ebcf039340c36589efef
Cristian Cordoba <cristian56cordoba@gmail.com>
2024-09-02 17:53:17 -0300

    Cambio de estructura, agregnado los metodos al archivo programs.cs

--- Program.cs
+++ Program.cs
@@ -19,10 +19,48 @@ internal class Program
     private static Cadeteria CrearCadeteria()
     {
         var Csv = LeerCsv("datosCadeteria.csv");
+
+        var datos = Csv[0].Split(",");
+
+        if(datos.Count() < 2)throw new Exception("No hay datos suficientes para instanciar la cadeteria");
+
+        return new Cadeteria(datos[0], datos[1]);
     }
 
-    private static List<string> LeerCsv(string v, bool cabecera = true)
+    private static void cargarPedidos(Cadeteria cadeteria){
+        var Csv = LeerCsv("DatosCadeteria.csv");
+
+        foreach (var linea in Csv)
+        {
+            var datos = linea.Split(",");
+            if (datos.Count() < 4)
+            {
+                System.Console.WriteLine($"\n[!] No se pudo cargar el cadete: {linea} - {datos}");
+                continue;
+            }
+
+            cadeteria.AltaCadete(new Cadete(int.Parse(datos[0]), datos[1], datos[2], datos[3]));
+        }
+    }
+
+    private static List<string> LeerCsv(string archivo, bool cabecera = true)
     {
         var lineas = new List<string>();
+
+        using (FileStream archivoCsv = new FileStream(archivo, FileMode.Open))
+        {
+            using (StreamReader readerCsv = new StreamReader(archivoCsv))
+            {
+                if (cabecera) readerCsv.ReadLine();
+
+                while (readerCsv.Peek() != -1)
+                {
+                    var linea = readerCsv.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(linea)) lineas.Add(linea);
+                }
+            }
+        }
+
+        return lineas;
     }
 }
